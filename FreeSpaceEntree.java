5fafbee6cfaaf32db5f6676177904f9cfdae6acc
AlexandraYepifanova <alexyepifanova269399@gmail.com>
2015-11-03 14:04:16 -0500

    Create FreeSpaceEntree.java

---
+++ FreeSpaceEntree.java
@@ -0,0 +1,8 @@
+public class FreeSpaceEntree{
+	public int block, size;
+	/*100k size and block is numbered 0-99*/
+	FreeSpaceEntree(int block, int size){
+		this.block=block;
+		this.size=size;
+	}
+}
