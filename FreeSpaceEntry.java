011f516f26b2aa04f26c7ee4fc36899b3dd2393c
Ahmed Reaz <ahmedreaz12@gmail.com>
2015-11-04 11:01:42 -0500

    Added sorting for FreeSpaceEntry table for Memory Management

--- FreeSpaceEntry.java
+++ FreeSpaceEntry.java
@@ -1,8 +1,48 @@
-public class FreeSpaceEntry{
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class FreeSpaceEntry implements Comparable<FreeSpaceEntry>{
+	
 	public int block, size;
 	/*100k size and block is numbered 0-99*/
 	FreeSpaceEntry(int block, int size){
 		this.block=block;
 		this.size=size;
 	}
+	
+	/* @toString: Always good for debugging
+	 * (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	public String toString(){
+		return "Block: "+block+"\tSize: "+size;
+	}	
+	
+	//Following variables/methods are used for Sorting The FreeSpace table
+	private static boolean sortBySize;
+	
+	public static void sortBySize(List<FreeSpaceEntry> tableToSort){
+		FreeSpaceEntry.sortBySize = true;
+		Collections.sort(tableToSort);
+	}
+	
+	public static void sortByBlock(List<FreeSpaceEntry> tableToSort){
+		FreeSpaceEntry.sortBySize = false;
+		Collections.sort(tableToSort);
+	}
+
+	@Override
+	public int compareTo(FreeSpaceEntry o) {
+		int comparableVar1, comparableVar2;
+		if(sortBySize){
+			comparableVar1 = this.size;
+			comparableVar2 = o.size;
+		}else{
+			comparableVar1 = this.block;
+			comparableVar2 = o.block;
+		}
+		return comparableVar1 < comparableVar2 ? -1 : comparableVar1 > comparableVar2 ? 1 : 0;
+	}
+	
 }
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # OS-Project
+Eclipse: How to start?
+Project is Eclipse Ready, that means we can continue development in eclipse. Just do a git pull on the remote and import existing project in Eclipse.
+
+Very important!
+Before pushing any changes to master, do a rebase with master and resolve the conflicts.
