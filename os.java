8f38a84daa02c2c328ea60543bbb01e509705de8
AlexandraYepifanova <alexyepifanova269399@gmail.com>
2015-11-10 20:27:50 -0500

    Update os.java
    -program keeps track of how much cpu time is used per program(saved in timeUsed variable of the Job class)
    -program makes sure that no more then one program is being moved into memory at a time(apparently a program can take up a lot of time being moved into memory from drum)
    -time slice variable added, and can be changed to any time slice
    -some minor mistakes corrected
    -program now works till job 7 comes in.
    current error: job 5 requests termination, but is still using I/O, and when it is done with I/O, it gets put back onto the cpuQueue

--- os.java
+++ os.java
@@ -4,16 +4,19 @@ public class os{
 	static LinkedList<Job> jobsInDrum;//is a list of jobs in the drum
 	static LinkedList<FreeSpaceEntry> freeSpaceTable;//is a list of free spaces entrees
 	static LinkedList<Job> jobsBeingSwapped;
-
+    
 	static Queue<Job> cpuQueue;//a queue of jobs on the cpuQueue
 	static Queue<Job> diskQueue;//contains jobs that are waiting to use the disk for I/O
-
+    
 	static Map<Integer, Job> jobsInMemory; //is List of all the jobs in memory
-
+    
 	
 	static boolean cpuRunningJob;//true or false whether a job is using the cpu at the moment
 	static boolean ioRunningJob; //true or false to indicate whether a job is using IO
-
+    static boolean jobMovingToMemory;//true or false if a job is being moved into memory
+    
+    static int timeslice=3;//*****************newly added 
+    
 	/*
 	 * Initialize the Variables. Think of it as power on for OS
 	 */
@@ -27,22 +30,22 @@ public class os{
 		cpuQueue=new LinkedList<Job>();
 		diskQueue=new LinkedList<Job>();
 	}
-
+    
 	/*
-	/when SOS has a new job moved to drum, Crint() is an interrupt that is called, and contains information about the job
-	/the job information is taken off the p[] array, and added to a job object
-	/the job object is added to a linkedList of jobs located in the drum called jobsInDrum
-	if this is the first job to enter the system, you can attempt to move job into memoery using tryMovingJobToMemory()
-	*/
+     /when SOS has a new job moved to drum, Crint() is an interrupt that is called, and contains information about the job
+     /the job information is taken off the p[] array, and added to a job object
+     /the job object is added to a linkedList of jobs located in the drum called jobsInDrum
+     if this is the first job to enter the system, you can attempt to move job into memoery using tryMovingJobToMemory()
+     */
 	static void Crint(int[] a, int[] p){
 		//@AR: Completed
 		Job newJobInDrum = new Job(p[1], p[2], p[3], p[4]); //Initialize the new job
 		jobsInDrum.add(newJobInDrum);	//Add job to list indicating jobs in the Drum
 		tryMovingJobToMemory();			//Try Moving the job into memory
-		scheduler(a,p);					//We will try to schedule the next Job
+        scheduler(a,p);					//We will try to schedule the next Job
 	}
-
-
+    
+    
 	//SOS calls this interrupt when a job was successfully moved from memory to disk(for I/O)
 	//this means you can try to put the next job on the diskQueue to use the disk
 	//also since Dskint was initiated, that means the program is not done with the CPU and has to be added back on the cpuQueue
@@ -69,19 +72,21 @@ public class os{
 		}
 		scheduler(a, p);
 	}
-
-
+    
+    
 	//SOS invokes this interrups when a job is loaded from drum into memory successfully
 	//therefore, this means the system is ready for another attempt to move something from drum into memory, and onto the cpuQueue
 	//and you can attempt to move something from the cpuQueue to using the cpu
 	//the scheduler() only attempts to make the move if nothing is running on the cpu at the moment by looking at the boolean jobOnCPU 
 	static void Drmint(int []a, int []p){
+        jobMovingToMemory=false;//because a job was just finished with laoding into memory*******************newly added
   		Job job = jobsBeingSwapped.remove();
-		FreeSpaceEntry.deleteEntry(job.block, job.jobSize, freeSpaceTable);
 		job.block = job.requestedBlock;
+        FreeSpaceEntry.deleteEntry(job.block, job.jobSize, freeSpaceTable);
 		cpuQueue.add(job);
 		jobsInMemory.put(job.jobNumber, job);
-		tryMovingJobToMemory();	// Try Moving the next job
+        if(jobsInDrum.size()>0)
+            tryMovingJobToMemory();	// Try Moving the next job
 		scheduler(a,p);			// try scheduling the job
 	}
 	
@@ -90,13 +95,13 @@ public class os{
 	//for now will be called when a process finishes with the cpu and wants to terminate, since the scheduler is in FCFS
 	static void Tro(int []a, int []p){
 		Job job = cpuQueue.peek();
+        job.usedTime+=timeslice;//*****************newly added 
 		if(job != null){
-			if(job.usedTime > job.maxCPUTime){
+			if(job.usedTime >= job.maxCPUTime){
 				//Job has used the maximum CPU TIME
-				//@TODO: This is a valid statement, but we don't have a valid
-				//way to know how much time the job has used.
-				terminate (job.jobNumber);
-			}else{
+                terminate (job.jobNumber);
+			}
+            else{
 				//Job has used its current timeslice
 				cpuQueue.poll();	//remove from top of the queue
 				cpuQueue.add(job);	//add at tail of the queue
@@ -111,6 +116,7 @@ public class os{
 	//when a=7 then job wants to be blocked (change boolean blocked, of current job, to true
 	//or else it is called when an I/O request is done
 	static void Svc(int[] a, int[] p){
+        cpuQueue.peek().usedTime+=p[5]-cpuQueue.peek().lastSceduledTime;//*****************newly added 
 		if(a[0]==5){
 			terminate(cpuQueue.peek().jobNumber);
 		}else if(a[0]==6){
@@ -127,14 +133,16 @@ public class os{
 			//First make sure that there is at least one IO request in queue fot this job
 			//If not, then ignore
 			for(Job j : diskQueue){
-				if(p[1] == j.jobNumber){
+				if(cpuQueue.peek().jobNumber == j.jobNumber){//************************changed
 					j.blocked = true;
-				}
+            }
+            cpuQueue.peek().blocked=true;//*******************newly added     not sure if even needed
+            cpuQueue.add(cpuQueue.poll());//*******************newly added     not sure if even needed
 			}
 		}
 		scheduler(a, p);
 	}
-
+    
 	//Termination
 	//Remove job from queues, if exist
 	static void terminate(int jobID) {
@@ -143,11 +151,13 @@ public class os{
 			job.killThisJob = true;
 		}else if(job !=  null){
 			freeSpaceTable.add(new FreeSpaceEntry(job.block, job.jobSize));
+            FreeSpaceEntry.compactBlocks(freeSpaceTable);//******************newly added
 			cpuQueue.remove(job);
 		}
+        tryMovingJobToMemory();//******************newly added
 	}
-
-
+    
+    
 	//this function attempts to move something from drum(backing store) to main memory
 	//The algorithm for picking a job from backing store, and try to fit into memory, is as follows:
 	//search for the first highest priority job in the drum
@@ -156,20 +166,22 @@ public class os{
 	//if space is not found, the job is added to a vector of jobs that can't fit into memory called jobsNotFitting.
 	//           and try the next highest priority job in the drum.
 	static boolean tryMovingJobToMemory(){
-		Collections.sort(jobsInDrum);
-		int base = -1;
-		for(Job job : jobsInDrum){
-			base = findSpace(job);
-			if(base >= 0){
-				jobsInDrum.remove(job);
-				swapToMemory(job, base);
-				return true;
-			}
-		}
-		return false;
+        if(!jobMovingToMemory){
+            Collections.sort(jobsInDrum);
+            int block = -1;
+            for(Job job : jobsInDrum){
+                block = findSpace(job);
+                if(block >= 0){
+                    jobsInDrum.remove(job);
+                    swapToMemory(job, block);
+                    return true;
+                }
+            }
+        }
+        return false;
 	}
-
-
+    
+    
 	//function takes a job passed to it(the job should be in the drum)
 	//takes the location of the job in the linkedList(jobsInDrum)
 	//takes the block/base that the job will be stored in main memory
@@ -179,9 +191,10 @@ public class os{
 	static void swapToMemory(Job jobInDrum, int block){
 		jobsBeingSwapped.add(jobInDrum);
 		jobInDrum.requestedBlock=block;
-		sos.siodrum(jobInDrum.jobNumber, jobInDrum.jobSize, block, 0);	
+        jobMovingToMemory=true;//**************************newly added
+		sos.siodrum(jobInDrum.jobNumber, jobInDrum.jobSize, block, 0);
 	}
-
+    
 	//checks the free space table if the job passed in the argument can fit a space in the free space table
 	//returns the block/base found for it, and updates the free space table to subtract the size of the job passed to it 
 	//if nothing found, returns a block/base value of -1(this value can never be)
@@ -193,31 +206,36 @@ public class os{
 		}
 		return -1;
 	}
-
-
+    
+    
 	//first come first served
 	//check if there is no job running on the cpu
 	//if there isn't, pop a job of the cpu queue, and run it on the cpu by changing a[0] to 2 	
 	static void scheduler(int []a, int []p){
-                		Job firstJob = cpuQueue.peek();
-                        Job jobToRun=firstJob;
-                        while(jobToRun !=null && jobToRun.blocked){
-                        	cpuQueue.add(cpuQueue.poll());
-                        	jobToRun = cpuQueue.peek();
-                        	if(jobToRun == firstJob){
-                        		jobToRun = null;
-                        	}
-                        }
-                        if(jobToRun!=null){
-                                a[0]=2;
-                                p[2]=jobToRun.block;
-                                p[3]=jobToRun.jobSize;
-                                p[4]=2;//jobToRun.maxCPUTime;
-                                cpuRunningJob=true;
-                                jobToRun.lastSceduledTime = p[5];
-                        }else{
-                        	a[0]=1;
-                        }
+        Job firstJob = cpuQueue.peek();
+        Job jobToRun=firstJob;
+        while(jobToRun !=null && jobToRun.blocked){
+            cpuQueue.add(cpuQueue.poll());
+            jobToRun = cpuQueue.peek();
+            if(jobToRun == firstJob){
+                jobToRun = null;
+            }
+        }
+        if(jobToRun!=null){
+            a[0]=2;
+            p[2]=jobToRun.block;
+            p[3]=jobToRun.jobSize;
+            if((jobToRun.maxCPUTime-jobToRun.usedTime)<timeslice)//*****************newly added 
+                p[4]=jobToRun.maxCPUTime-jobToRun.usedTime;//*****************newly added 
+            else
+                p[4]=timeslice;
+            cpuRunningJob=true;
+            jobToRun.lastSceduledTime = p[5];
+            
+        }else{
+            a[0]=1;
+        }
 	}
+    
 	
 }
